c6a3743613829f544eb483702a44d5a46f0c3480
MDromel030 <mdfahadbinazam@gmail.com>
2026-05-09 22:53:58 +0600

    Java 3rd Java Control Statements--changed the variable name and logic also

--- src/Classtwo/LogicalOperator.java
+++ src/Classtwo/LogicalOperator.java
@@ -6,7 +6,7 @@ public class LogicalOperator {
 
         int number_one = 100, number_two = 200;
 
-        System.out.println("a && b: " + (a && b));
-        System.out.println("a || b: " + (a || b));
+        System.out.println("a && b: " + (number_one > number_two && number_one < number_two));
+        System.out.println("a || b: " + (number_one == number_two && number_one > number_two));
     }
 }
