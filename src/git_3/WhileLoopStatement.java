5b86f3d62e24682aae68c94d7269fe7d3afed841
MDromel030 <mdfahadbinazam@gmail.com>
2026-05-12 18:29:10 +0600

    Add WhileLoopStatement class implementation

---
+++ src/git_3/WhileLoopStatement.java
@@ -0,0 +1,28 @@
+package git_3;
+
+public class WhileLoopStatement {
+
+    public static void main(String[] args) {
+
+        // while loop
+        System.out.println("While Loop:");
+
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+
+        // do-while loop
+        System.out.println("\nDo While Loop:");
+
+        int j = 1;
+
+        do {
+            System.out.println(j);
+            j++;
+        } while (j <= 5);
+    }
+}
+
